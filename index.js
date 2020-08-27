6261d9db028289fd2045a8fd47a8c28fd5d9e9bf
Ayush Kumar Singh <45496026+Ayush-KS@users.noreply.github.com>
2020-08-27 19:08:36 +0530

    Add keypresses

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ testInstance instanceof AudioSynth; // true
 testInstance === Synth; // true
 
 
-
 $(".key").click(function() { 
     var forms = $(".form");
     var piano;
@@ -28,6 +27,43 @@ $(".key").click(function() {
     if(inst == "organ" || inst == "edm")
         duration = 1;
     piano.play(note, octave, duration); 
+})
+
+function isValid(note) {
+    switch(note) {
+        case 'A' : return true;
+        case 'B' : return true;
+        case 'C' : return true;
+        case 'D' : return true;
+        case 'E' : return true;
+        case 'F' : return true;
+        case 'G' : return true;
+        default : return false;
+    }
+}
+
+$("body").keydown(function(e) {
+    var note = String.fromCharCode((e.which));
+    console.log(note);
+
+    var forms = $(".form");
+    var piano;
+    var inst = "piano";
+    for(let i = 0; i < forms.length; i++) {
+        //console.log(forms[i].innerText)
+        if($(forms[i]).hasClass("full-opacity")) {
+            piano = Synth.createInstrument((forms[i].innerText).toLowerCase());
+            inst = (forms[i].innerText).toLowerCase();
+            break;
+        }
+    }
+    var duration = 2;
+    if(inst == "organ" || inst == "edm")
+        duration = 1;
+    if(!isValid(note)) {
+        note = String.fromCharCode((65 + e.which % 7));;
+    }
+    piano.play(note, 4, duration); 
 })
 
 $(".form").click(function() {
